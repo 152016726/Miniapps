9490727e27600dd4eee3836496b1a8ccfe012368
owen <941167620@qq.com>
2018-06-13 15:04:21 +0800

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var server=require ('./pages/modules/server')
 
 App({
   onLaunch: function () {
-    console.log('onLaunch..............')
+    console.log('onLaunch111..............')
   var that=this;
   },
   data: {
